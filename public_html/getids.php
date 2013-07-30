ea2c59bd93c932afbb3a085172066064b9490188
Dan Michael Heggø <danmichaelo@gmail.com>
2013-07-30 11:34:52 +0200

    Separate ISBN lookup code (getobjektids) from Bibsys ID lookups (getids)

--- public_html/getids.php
+++ public_html/getids.php
@@ -5,7 +5,8 @@ ini_set('display_errors', '1');
 
 function usage() {
     header('Content-type: text/plain; charset=utf-8');
-	print "Bruk: \n\n    " . $_SERVER['PHP_SELF'] ."?id=<id>\n\nder <id> er et knyttid eller dokid, eller\n\n    " . $_SERVER['PHP_SELF'] ."?isbn=<isbn>\n\nder <isbn> er et isbn-nummer. Eksempel:\n\n    " . $_SERVER['PHP_SELF'] ."?id=036051NA0\n\nFor å få resultatene i JSONP istedetfor JSON; bruk callback. Eksempel:\n\n    " . $_SERVER['PHP_SELF'] ."?id=036051NA0&callback=minfunksjon\n";
+    $me = $_SERVER['HTTPS'] ? 'https://' : 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+	print "Bruk: \n\n    " . $me ."?id=<id>\n\nder <id> er et knyttid eller dokid. Eksempel:\n\n    " . $me ."?id=036051NA0\n\nFor å få resultatene i JSONP istedetfor JSON; bruk callback. Eksempel:\n\n    " . $me ."?id=036051NA0&callback=minfunksjon\n";
 	exit();	
 }
 
@@ -20,17 +21,6 @@ function file_get_contents2($url) {
     return $data;
 }
 
-function make_query($cql, $start = 1, $count = 10) {
-    return http_build_query(array(
-        'version' => '1.2',
-        'operation' => 'searchRetrieve',
-        'recordSchema' => 'marcxchange', // "bibsysmarc" (ikke offisielt støttet) eller "marcxchange"
-        'startRecord' =>  $start,
-        'maximumRecords' => $count,
-        'query' => $cql
-    ));
-}
-
 function return_json($obj) {
     if (isset($_REQUEST['callback'])) {
         header('Content-type: application/javascript; charset=utf-8');
@@ -43,73 +33,26 @@ function return_json($obj) {
     }
 }
 
+if (!isset($_GET['id'])) usage();
+$knyttdokid = $_GET['id'];
+if (empty($knyttdokid)) usage();
 
-if (isset($_GET['id'])) {
+$url = 'http://adminwebservices.bibsys.no/objectIdService/getObjectId?id=' . $knyttdokid;
+$url = 'http://adminwebservices.bibsys.no/objectIdService/getIds?id=' . $knyttdokid;
 
-    $knyttdokid = $_GET['id'];
-    if (empty($knyttdokid)) usage();
+$ids = trim(file_get_contents2($url));
 
-    $url = 'http://adminwebservices.bibsys.no/objectIdService/getObjectId?id=' . $knyttdokid;
-    $url = 'http://adminwebservices.bibsys.no/objectIdService/getIds?id=' . $knyttdokid;
-
-    $ids = trim(file_get_contents2($url));
-
-    $json = array();
-    $keys = array(
-        'objektId' => 'objektid',
-        'dokumentId' => 'dokid',
-        'hefteId' => 'heftid',
-    );
-    foreach (explode("\n", $ids) as $line) {
-        list($key, $val) = explode(':', $line);
-        $json[$keys[$key]] = trim($val);
-    }
-
-    return_json($json);
-
-} else if (isset($_GET['isbn'])) {
-
-    $isbn = $_GET['isbn'];
-    if (empty($isbn)) usage();
-
-    $qs = make_query('bs.isbn="' . addslashes($isbn) . '"');
-
-    $baseurl = 'http://sru.bibsys.no/search/biblio?';
-    #$baseurl = 'http://utvikle-a.bibsys.no/search/biblio?';
-
-    $source = file_get_contents2("$baseurl$qs");
-
-    //print $source;
-
-    $ns = array(
-        'srw' => 'http://www.loc.gov/zing/srw/',
-        'marc' => 'info:lc/xmlns/marcxchange-v1'
-    );
-
-    $json = array('isbn' => $isbn);
-
-    //print $source;
-
-    $xml = new SimpleXMLElement($source);
-    $xml->registerXPathNamespace('srw', $ns['srw']);
-    $xml->registerXPathNamespace('marc', $ns['marc']);
-
-    //$output['numberOfRecords'] = intval(current($xml->xpath('/srw:searchRetrieveResponse/srw:numberOfRecords')));
-
-    $diag = $xml->xpath('//srw:diagnostics');
-
-    if (count($diag) != 0) {
-        return_json(array('error' => strval($diag[0]->diagnostic->message)));
-    }
-
-    foreach ($xml->xpath("/srw:searchRetrieveResponse/srw:records/srw:record") as $record) {
-        $rec_srw = $record->children($ns['srw']);
-        $json['objektid'] = strval($rec_srw->recordIdentifier);
-    }
-    return_json($json);
-
-} else {
-    usage();
+$json = array();
+$keys = array(
+    'objektId' => 'objektid',
+    'dokumentId' => 'dokid',
+    'hefteId' => 'heftid',
+);
+foreach (explode("\n", $ids) as $line) {
+    list($key, $val) = explode(':', $line);
+    $json[$keys[$key]] = trim($val);
 }
+
+return_json($json);
 
 ?>
---
+++ public_html/getobjektids.php
@@ -0,0 +1,117 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require_once('../customxmlelement/customxmlelement.php');
+
+function usage() {
+    header('Content-type: text/plain; charset=utf-8');
+    $me = $_SERVER['HTTPS'] ? 'https://' : 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+	print 'Bruk: '
+        . "\n\n" 
+        . '    ' . $me . '?isbn=<isbn>'
+        . "\n\n"
+        . 'der <isbn> er et isbn-nr. Eksempel:'
+        . "\n\n"
+        . '    ' . $me . '?isbn=9810223870'
+        . "\n\n" 
+        . 'For å få resultatene i JSONP istedetfor JSON; bruk callback. Eksempel:'
+        . "\n\n"
+        . '    ' . $me . "?isbn=9810223870&callback=minfunksjon"
+        . "\n";
+	exit();	
+}
+
+function file_get_contents2($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'UBO Scriptotek Dalek');
+    curl_setopt($ch, CURLOPT_HEADER, 0); // no headers in the output
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return instead of output
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+function make_query($cql, $start = 1, $count = 10) {
+    return http_build_query(array(
+        'version' => '1.2',
+        'operation' => 'searchRetrieve',
+        'recordSchema' => 'marcxchange', // "bibsysmarc" (ikke offisielt støttet) eller "marcxchange"
+        'startRecord' =>  $start,
+        'maximumRecords' => $count,
+        'query' => $cql
+    ));
+}
+
+function return_json($obj) {
+    if (isset($_REQUEST['callback'])) {
+        header('Content-type: application/javascript; charset=utf-8');
+        echo $_REQUEST['callback'] . '(' . json_encode($obj) . ')';
+        exit();
+    } else {
+        header('Content-type: application/json; charset=utf-8');
+        echo json_encode($obj);
+        exit();
+    }
+}
+if (!isset($_GET['isbn'])) usage();
+$isbn = $_GET['isbn'];
+if (empty($isbn)) usage();
+
+$qs = make_query('bs.isbn="' . addslashes($isbn) . '"');
+
+$baseurl = 'http://sru.bibsys.no/search/biblio?';
+#$baseurl = 'http://utvikle-a.bibsys.no/search/biblio?';
+
+$uri = $baseurl . $qs;
+$source = file_get_contents2($uri);
+
+//print $source;
+
+$ns = array(
+    'srw' => 'http://www.loc.gov/zing/srw/',
+    'marc' => 'info:lc/xmlns/marcxchange-v1'
+);
+
+$json = array('isbn' => $isbn);
+
+//print $source;
+
+$xml = new CustomXmlElement($source);
+$xml->registerXPathNamespaces(array(
+    'srw' => 'http://www.loc.gov/zing/srw/',
+    'marc' => 'http://www.loc.gov/MARC21/slim',
+    'd' => 'http://www.loc.gov/zing/srw/diagnostic/'
+));
+
+//$output['numberOfRecords'] = intval(current($xml->xpath('/srw:searchRetrieveResponse/srw:numberOfRecords')));
+
+$diag = $xml->xpath('//srw:diagnostics');
+
+if (count($diag) != 0) {
+    return_json(array('error' => strval($diag[0]->diagnostic->message)));
+}
+$json['source'] = $uri;
+$json['objects'] = array();
+foreach ($xml->xpath("/srw:searchRetrieveResponse/srw:records/srw:record") as $record) {
+    $obj = array();
+
+    // Id (objektid)
+    $obj['id'] = $record->text('srw:recordIdentifier');
+
+    // Isbn
+    $obj['isbn'] = $record->text('srw:recordData/marc:record/marc:datafield[@tag="020"]/marc:subfield[@code="a"]');
+
+    // Is object an electronic resource or not?
+    preg_match('/elektronisk ressurs/', 
+               $record->text('srw:recordData/marc:record/marc:datafield[@tag="245"]/marc:subfield[@code="h"]'), 
+               $matches);
+    $obj['electronic'] = $matches ? true : false; 
+
+    $json['objects'][] = $obj;
+}
+return_json($json);
+
+?>
