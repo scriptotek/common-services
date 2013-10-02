2ca8ed7fe19292f4039886db4d6781c5ba8d2a7b
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-10-02 20:39:30 +0000

    Add EasyRDF as dependency

--- public_html/deweyinfo.php
+++ public_html/deweyinfo.php
@@ -1,12 +1,18 @@
 <?php
 
-require_once "easyrdf-0.7.2/lib/EasyRdf.php";
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+#require_once "easyrdf-0.7.2/lib/EasyRdf.php";
 #require_once "html_tag_helpers.php";
+
+require_once('../vendor/autoload.php');
+require_once('../vendor/easyrdf/easyrdf/lib/EasyRdf.php');
 
 function usage() {
     header('Content-type: text/plain;charset=UTF-8');
     print "Bruk: \n\n    " . $_SERVER['PHP_SELF'] ."?ddc=<ddc> der <ddc> er et Dewey-nummer. Eksempel:\n\n    " . $_SERVER['PHP_SELF'] ."?ddc=539.7258\n\nFor å få resultatene i JSONP istedetfor JSON; bruk callback. Eksempel:\n\n    " . $_SERVER['PHP_SELF'] ."?ddc=539.7258&callback=minfunksjon\n";
-    exit(); 
+    exit();
 }
 
 function return_json($obj) {
